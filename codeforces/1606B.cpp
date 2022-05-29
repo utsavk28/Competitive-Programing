5b801f2e753d8f8ac46da4cd31cb465167062fb6
Utsav Khatu <62751545+utsavk28@users.noreply.github.com>
2022-05-29 19:05:10 +0530

    Create 1606B.cpp

---
+++ codeforces/1606B.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define mod 1000000007
+
+class Node {
+    public:
+    ll val;
+    vector<Node*> adj;
+    Node *left,*right;
+    Node(int x) {
+        val = x;
+        adj.clear();
+    }
+};
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll to_number(string s) {
+    ll ans = 0;
+    int n = s.size(),i;
+    for(i=0;i<n;i++) {
+        ans = ans*10 + (s[i]-'0');
+    }
+    
+    return ans;
+}
+
+ll power(ll x, ll y, ll p)
+{
+    ll res = 1;     
+    x = x % p; 
+    if (x == 0) return 0; 
+    while (y > 0)
+    {
+        if (y & 1)
+            res = (res*x) % p;
+ 
+        y = y>>1;
+        x = (x*x) % p;
+    }
+    return res;
+}
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); 
+    
+    int t,cs=0;
+    cin>>t;
+    while(t--) {
+        cs++;
+        ll n,k,ans = 0,m = 1;
+        cin>>n>>k;
+        while(m<=k) {
+            m *= 2;
+            ans++;
+        }
+        ans += ceil((1.0*n-m)/k);
+        // cout<<"Case #"<<cs<<": ";
+        cout<<ans<<endl;
+    }
+	return 0;
+}
